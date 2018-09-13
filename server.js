216616c0913124b91dd9e4330e96a3594982ef59
Xin Dou <master2dou@gmail.com>
2018-09-13 21:26:37 +0800

    clean code

--- server.js
+++ server.js
@@ -3,41 +3,32 @@ const transfer = require('./transfer');
 
 const rootServer = {
     '/transfer': req => {
-        const { shareId, accountName, amount, memo } = req.json();
-        const { key } = req.query;
-        if (key !== '81929a8f-8c28-4d9b-be3a-fff25bb6f21a') {
-            req.response.json({
-                code: -1,
-                errMsg: 'no permission',
-            });
-        } else {
-            try {
-                const result = transfer.transferTo(accountName, amount, memo);
-                console.log(result);
-                if (result && result['transaction_id']) {
-                    req.response.json({
-                        code: 0,
-                        data: {
-                            shareId,
-                            accountName,
-                            amount,
-                            memo,
-                            transferResult: result
-                        }
-                    });
-                } else {
-                    req.response.json({
-                        code: -1,
-                        errMsg: 'transfer failed'
-                    });
-                }
-            } catch (e) {
+        const { accountName, amount, memo } = req.json();
+
+        try {
+            const result = transfer.transferTo(accountName, amount, memo);
+            console.log(result);
+            if (result && result['transaction_id']) {
+                req.response.json({
+                    code: 0,
+                    data: {
+                        accountName,
+                        amount,
+                        memo,
+                        transferResult: result
+                    }
+                });
+            } else {
                 req.response.json({
                     code: -1,
-                    errMsg: e
+                    errMsg: 'transfer failed'
                 });
             }
-
+        } catch (e) {
+            req.response.json({
+                code: -1,
+                errMsg: e
+            });
         }
     }
 };
